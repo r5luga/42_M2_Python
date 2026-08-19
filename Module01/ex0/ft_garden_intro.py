8f8ce244d7a542d863f512ad3b315181cc0eab6a
LNO <luisnuno@hotmail.com>
2026-08-19 13:28:27 +0100

    20260819

---
+++ Module01/ex0/ft_garden_intro.py
@@ -0,0 +1,17 @@
+"""
+#!/usr/bin/env python3
+"""
+
+def main():
+    name = "Rose"
+    height = "25cm"
+    age = "30 days"
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}")
+    print(f"Age: {age}")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    main()
